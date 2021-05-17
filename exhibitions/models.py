4cb9380de9da07809b15b88b494ee4d2c4d4d63a
Yariv Friedman <yrfriedman@gmail.com>
2021-05-17 10:53:24 +0200

    Merge remote before blog test

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Photo(models.Model):
     title = models.CharField(max_length=60, default='', blank=True)
-    description = models.TextField(max_length=200, default='', blank=True)
+    description = models.TextField(default='', blank=True)
     width = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
     image = models.ImageField(width_field="width", height_field="height")
